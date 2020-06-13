f2b3b079b610dba3281af3ea02343a2aa64e8185
ivankahnybediuk <56602081+ivankahnybediuk@users.noreply.github.com>
2020-06-13 18:18:16 +0300

    5

--- scripts/index.js
+++ scripts/index.js
@@ -8,6 +8,24 @@
 $(document).ready(function(){
 new CircleType((document.getElementById('circle-text'))).radius(80)
 
+const paralaxContainer = document.querySelectorAll(".paralax-container")
+paralaxContainer.forEach(element=>{
+  element.addEventListener("mousemove", function(event){
+   $(".paralax").css("transform", `translate(${event.screenX/50}px)`)
+  
+  })
+})
+
+
 
 })
+$('.third-page').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  arrows: false
+});
+
+		
 
